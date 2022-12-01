e7c01bbff92fe9a658b7c12a4205210eb994ae19
holdena1 <holden.adamec@gmail.com>
2022-11-30 18:17:09 -0800

    Finished sollouette test

--- src/RayTracer.cpp
+++ src/RayTracer.cpp
@@ -28,11 +28,15 @@ Intersection RayTracer::Intersect(Ray ray, Triangle triangle) {
     Intersection hit;
     glm::mat4 x = inverse(glm::mat4(glm::vec4(triangle.P[0], 1), glm::vec4(triangle.P[1], 1), glm::vec4(triangle.P[2], 1), glm::vec4(-ray.dir, 0)));
     glm::vec4 y = x * glm::vec4(ray.p0, 1);
-    hit.dist = y[3];
-    hit.P = y[0] * triangle.P[0] + y[1] * triangle.P[1] + y[2] * triangle.P[2];
-    hit.N = normalize(y[0] * triangle.N[0] + y[1] * triangle.N[1] + y[2] * triangle.N[2]);
-    hit.triangle = &triangle;
-    hit.V = ray.dir;
+    if (y[0] < 0 || y[1] < 0 || y[2] < 0) {
+        hit.dist = std::numeric_limits<float>::infinity();
+    } else {
+        hit.dist = y[3];
+        hit.P = y[0] * triangle.P[0] + y[1] * triangle.P[1] + y[2] * triangle.P[2];
+        hit.N = normalize(y[0] * triangle.N[0] + y[1] * triangle.N[1] + y[2] * triangle.N[2]);
+        hit.triangle = &triangle;
+        hit.V = ray.dir;
+    }
     return hit;
 }
 
@@ -50,22 +54,23 @@ Intersection RayTracer::Intersect(Ray ray, RTScene scene) {
 }
 
 glm::vec3 RayTracer::FindColor(Intersection hit, int recursion_depth) {
-    glm::vec3 color = glm::vec3(0, 0, 0);
-    if (!hit.hit) return glm::vec3(0, 0, 0);
+    glm::vec3 color = glm::vec3(1, 1, 1);
+    if (hit.dist == std::numeric_limits<float>::infinity()) color = glm::vec3(0, 0, 0);
+    // if (!hit.hit) return glm::vec3(0, 0, 0);
 
-    if (hit.material->texture) {
-        color = hit.material->texture->GetColor(hit.texCoord.x, hit.texCoord.y);
-    }
-    else {
-        color = hit.material->diffuse;
-    }
+    // if (hit.material->texture) {
+    //     color = hit.material->texture->GetColor(hit.texCoord.x, hit.texCoord.y);
+    // }
+    // else {
+    //     color = hit.material->diffuse;
+    // }
 
-    if (hit.material->reflective && recursion_depth < 2) {
-        Ray reflectRay;
-        reflectRay.p0 = hit.point;
-        reflectRay.dir = glm::normalize(hit.dir - 2 * glm::dot(hit.dir, hit.normal) * hit.normal);
-        Intersection reflectHit = Intersect(reflectRay, *hit.scene);
-        color += hit.material->reflective * FindColor(reflectHit, recursion_depth + 1);
-    }
+    // if (hit.material->reflective && recursion_depth < 2) {
+    //     Ray reflectRay;
+    //     reflectRay.p0 = hit.point;
+    //     reflectRay.dir = glm::normalize(hit.dir - 2 * glm::dot(hit.dir, hit.normal) * hit.normal);
+    //     Intersection reflectHit = Intersect(reflectRay, *hit.scene);
+    //     color += hit.material->reflective * FindColor(reflectHit, recursion_depth + 1);
+    // }
     return color;
 }
