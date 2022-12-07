2c898aab8ff1df44ca3abdc4c9143202e12b25e1
Harrison K <53415491+kungharrison@users.noreply.github.com>
2022-12-07 10:15:43 -0800

    added indicies for RTCube

--- include/RTCube.h
+++ include/RTCube.h
@@ -36,7 +36,15 @@ public:
             // Bottom face
             { 0.0f, -1.0f, 0.0f },{ 0.0f, -1.0f, 0.0f },{ 0.0f, -1.0f, 0.0f },{ 0.0f, -1.0f, 0.0f }
         };
-
+        // Cube indices
+        const GLuint indices[36] = {
+            0, 1, 2, 0, 2, 3, // Front face
+            4, 5, 6, 4, 6, 7, // Back face
+            8, 9, 10, 8, 10, 11, // Left face
+            12, 13, 14, 12, 14, 15, // Right face
+            16, 17, 18, 16, 18, 19, // Top face
+            20, 21, 22, 20, 22, 23 // Bottom face
+        };
         // convert above GLfloats into vector of all triangles
         		
         for (int i = 0; i < 24; i+=4) {
